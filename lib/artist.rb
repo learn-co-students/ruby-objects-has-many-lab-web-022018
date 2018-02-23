fb992970451a9678107ff31498dc0f953440611c
github username <github email address>
2018-02-22 20:19:03 -0500

    Created artist, author, post, song

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist= self
+    @@song_count += 1
+    song
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+
+end
